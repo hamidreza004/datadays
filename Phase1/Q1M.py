20d21bb39fc62a6aa35d8d51cd7906f2e90aa9ff
hamidreza <hamidreza004@gmail.com>
2019-02-03 03:16:02 +0330

    write final short question

--- Phase1/Q1M.py
+++ Phase1/Q1M.py
@@ -11,10 +11,18 @@ def main():
     filename = sys.argv[1] if len(sys.argv) > 1 else 'output3.csv'
     data = pd.read_csv(filename, lineterminator="\n")
     arr = Counter("")
+    cnt = 0
+    sep = ".,،:;"
     for x in data.desc:
-        arr.add(Counter(x.split()))
-    arr.most_common(100)
-    print("end")
+        for c in sep:
+            x = x.replace(c, ' ')
+        if cnt % 100 == 0:
+            print(cnt)
+        cnt += 1
+        arr += Counter(x.split())
+        if len(arr) > 1000000:
+            arr = arr.most_common(100000)
+    print(arr.most_common(100))
 
 
 main()
