12a9d8fe33064cbae8c516c215e80e430df82418
hamidreza <hamidreza004@gmail.com>
2019-02-03 03:07:33 +0330

    write final short question

--- Phase1/Q1M.py
+++ Phase1/Q1M.py
@@ -1,13 +1,20 @@
 import sys
-import string
 import pandas as pd
-import numpy as np
-from google import google
+from collections import Counter
+
+
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
 
 def main():
-
-    filename = sys.argv[1] if len(sys.argv) > 1 else 'divar_posts_dataset.csv'
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'output3.csv'
     data = pd.read_csv(filename, lineterminator="\n")
+    arr = Counter("")
+    for x in data.desc:
+        arr.add(Counter(x.split()))
+    arr.most_common(100)
     print("end")
+
 
 main()
