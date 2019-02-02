cfa8e199096017c4a27396c6196266d3a3712157
hamidreza <hamidreza004@gmail.com>
2019-02-03 00:43:39 +0330

    add opening file

---
+++ Phase1/Q1M.py
@@ -0,0 +1,10 @@
+import sys
+import string
+import pandas as pd
+import numpy as np
+from google import google
+
+def main():
+
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'sample_mobile_data_1000.csv'
+    data = pd.read_csv(filename)
